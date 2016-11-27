3cd1ce44dc7d40b387ea7398d2f5dc55873b69e0
mr433 <max.rodewald@gmail.com>
2016-11-27 13:16:58 -0700

    Reorganized layout/routing to be cleaner

--- src/js/client.js
+++ src/js/client.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-//import Layout from "./components/Layout";
 import MessageView from './components/Messages/MessageView';
 import HomeView from './components/Home/HomeView';
 import NavBar from './components/NavBar';
@@ -12,7 +11,7 @@ const app = document.getElementById('app');
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path='/' component={NavBar}>
-            <IndexRoute path='home' component={HomeView}></IndexRoute>
+            <IndexRoute component={HomeView}></IndexRoute>
             <Route path='messages' component={MessageView}></Route>
         </Route>
     </Router>
