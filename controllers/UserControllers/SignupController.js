813615dba36b54d647a853c1ee414cb783abba02
mr433 <max.rodewald@gmail.com>
2016-12-01 15:18:48 -0700

    Fixed bug that made it look like an account was created when an identical email address was provided to one already in the db.

--- controllers/UserControllers/SignupController.js
+++ controllers/UserControllers/SignupController.js
@@ -1,16 +1,18 @@
 var app = require('../../server.js');
 
 module.exports = {
-    
+
     //goes to database,
     //creates user with given values
     signup: function(req, res, next){
         var db = app.get('db');
         db.create_user([req.body.name, req.body.email, req.body.password], function(err, user){
             if(err){
+                console.log(err);
                 res.send(err).status(500);
                 return;
             }
+            console.log(user);
             res.sendStatus(200);
         });
     }
