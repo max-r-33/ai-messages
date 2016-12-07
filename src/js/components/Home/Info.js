7491fa9870df786ebcd458e336ff87e0ac4dd890
mr433 <max.rodewald@gmail.com>
2016-12-06 17:54:11 -0700

    Added some basic homepage info and styling.

--- src/js/components/Home/Info.js
+++ src/js/components/Home/Info.js
@@ -1,18 +1,101 @@
 import React from 'react';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
+import Message from '../Messages/MessageView/MessageContainer/Message.js';
 
 export default class Info extends React.Component {
+    scroll() {
+        document.querySelector('.examples').scrollIntoView({behavior: 'smooth'});
+    }
     render() {
         return (
-            <div className='info'>
-                <div className='infoArea'>
-                    <h1 className='heading'>Hello.</h1>
-                    <div className='subHeading'>
-                        <p>Finding basketball statistics has never been this easy.</p>
-                        <div className='formBtn formBtnLight'><Link to='signup'>Sign Up</Link></div>
+            <div>
+                <div className='infoBox'>
+                    <div className='homeCard'>
+                        <h1 className='heading'>Hello.</h1>
+                        <div className='subHeading'>
+                            <p>Finding basketball statistics has never been this easy.</p>
+                            <div className='formBtn formBtnLight'>
+                                <Link to='signup'>Sign Up</Link>
+                            </div>
+                        </div>
+                        <div onClick={this.scroll} className='arrow'>↓</div>
                     </div>
                 </div>
-                <div className='arrow'>↓</div>
+                <div className='infoBox'>
+                    <div className='examples'>
+                        <h1>Here are just a few of things you can ask</h1>
+                        <div className='examplesContainerGrid'>
+                            <div className='examplesContainerColumn'>
+                                <div className='exampleContainer'>
+                                    <h3 className='exampleContainerHeader'>
+                                        Teams
+                                    </h3>
+                                    <p className='exampleText'>
+                                        Did the lakers win?
+                                    </p>
+                                    <p className='exampleText'>
+                                        When do the cavs play again?
+                                    </p>
+                                    <p className='exampleText'>
+                                        Whats the dubs record?
+                                    </p>
+                                </div>
+                                <div className='exampleContainer'>
+                                    <h3 className='exampleContainerHeader'>
+                                        Statistics
+                                    </h3>
+                                    <p className='exampleText'>
+                                        How many points did Jordan Clarkson have?
+                                    </p>
+                                    <p className='exampleText'>
+                                        Who leads the league in mpg?
+                                    </p>
+                                    <p className='exampleText'>
+                                        Who is leading the league in assists to turnover ratio?
+                                    </p>
+                                </div>
+                            </div>
+                            <div className='examplesContainerColumn'>
+                                <div className='exampleContainer'>
+                                    <h3 className='exampleContainerHeader'>
+                                        League
+                                    </h3>
+                                    <p className='exampleText'>
+                                        Show me standings for the eastern conference?
+                                    </p>
+                                    <p className='exampleText'>
+                                        What games are on tomorrow?
+                                    </p>
+                                    <p className='exampleText'>
+                                        Whos playing tonight?
+                                    </p>
+                                </div>
+                                <div className='exampleContainer'>
+                                    <h3 className='exampleContainerHeader'>
+                                        Miscallaneous
+                                    </h3>
+                                    <p className='exampleText'>
+                                        Whats the weather in LA?
+                                    </p>
+                                    <p className='exampleText'>
+                                        Whats the price of apple stock?
+                                    </p>
+                                    <p className='exampleText'>
+                                        Hows the weather going to be tomorrow in Sydney?
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='infoBox'>
+                    <div className='homeCard'>
+                        <h1>Interested?</h1>
+                        <div className='formBtn formBtnLight'>
+                            <Link to='signup'>Sign Up</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
